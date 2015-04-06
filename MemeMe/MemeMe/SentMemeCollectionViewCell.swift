74268400fbf1f648a74d48fa36d9aa347c27bbbd
mikelA47 <lizarraldecm@gmail.com>
2015-04-07 01:37:43 +0200

    Committed by Udacity

---
+++ MemeMe/MemeMe/SentMemeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SentMemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Mikel Lizarralde Cabrejas on 3/4/15.
+//  Copyright (c) 2015 TheUnit. All rights reserved.
+//
+
+import UIKit
+
+class SentMemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memeImage: UIImageView!
+}
